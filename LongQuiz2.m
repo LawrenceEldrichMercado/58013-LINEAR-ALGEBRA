6939a0100c3de0e6bd931148e95275a693aae04b
Eldrich Mercado <144584971+LawrenceEldrichMercado@users.noreply.github.com>
2023-10-23 09:13:34 +0800

    Add files via upload

---
+++ LongQuiz2.m
@@ -0,0 +1,14 @@
+syms x y z;
+A = [1 2 3; 4 5 6; 7 8 9];
+At = A
+A_plus_A = A + A;
+A_times_A = A * A;
+det_A = det (A);
+A_plus_A
+A_times_A
+det_A
+syms x y z;
+A = [1 2 3; 4 5 6; 7 8 9];
+Submatrix = x(1:3, 2:3);
+Submatrix
+A = [1 2 3; 4 5 6; 7 8 9];
